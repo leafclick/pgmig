2b343e59ed3fcf5d158493e8242b9d8fb3d84e42
Kamil Toman <kamil.toman@leafclick.com>
2019-12-27 10:25:43 +0100

    Prepare for the next development iteration.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.leafclick/pgmig "0.2.0"
+(defproject com.leafclick/pgmig "0.2.1-SNAPSHOT"
 
   :description "Standalone PostgreSQL Migration Runner"
   :url "https://github.com/leafclick/pgmig"
