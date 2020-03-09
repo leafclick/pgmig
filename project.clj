7d8da74e0de1aa8c2fc48ea5d8f8f181cc6128be
Kamil Toman <kamil.toman@leafclick.com>
2020-03-09 12:19:21 +0100

    Release 0.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.leafclick/pgmig "0.2.1-SNAPSHOT"
+(defproject com.leafclick/pgmig "0.2.1"
 
   :description "Standalone PostgreSQL Migration Runner"
   :url "https://github.com/leafclick/pgmig"
