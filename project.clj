1094f9a5b46e02b308a30f81057b627394e84cd0
Kamil Toman <kamil.toman@leafclick.com>
2022-03-10 16:22:50 +0100

    Update project dependencies.
    Signed-off-by: Kamil Toman <kamil.toman@leafclick.com>

--- project.clj
+++ project.clj
@@ -14,22 +14,22 @@
                  [environ/environ "1.2.0"]
                  [com.taoensso/timbre "5.1.2"]
                  [com.fzakaria/slf4j-timbre "0.3.21" :exclusions [org.clojure/clojure]]
-                 [org.slf4j/slf4j-api "1.7.30"]
-                 [org.slf4j/log4j-over-slf4j "1.7.30"]
-                 [org.slf4j/jul-to-slf4j "1.7.30"]
-                 [org.slf4j/jcl-over-slf4j "1.7.30"]
-                 [org.clojure/tools.logging "1.1.0"]
+                 [org.slf4j/slf4j-api "1.7.36"]
+                 [org.slf4j/log4j-over-slf4j "1.7.36"]
+                 [org.slf4j/jul-to-slf4j "1.7.36"]
+                 [org.slf4j/jcl-over-slf4j "1.7.36"]
+                 [org.clojure/tools.logging "1.2.4"]
                  [org.clojure/java.jdbc "0.7.12"]
                  [org.clojure/tools.cli "1.0.206"]
                  [com.leafclick/hikariCP "HikariCP-3.3.2-graal-RC4"]
-                 [org.postgresql/postgresql "42.2.19"]
-                 [migratus "1.3.5" :exclusions [org.clojure/clojure]]
-                 [seancorfield/next.jdbc "1.1.646"]
+                 [org.postgresql/postgresql "42.3.3"]
+                 [migratus "1.3.6" :exclusions [org.clojure/clojure]]
+                 [seancorfield/next.jdbc "1.2.659"]
                  [camel-snake-kebab/camel-snake-kebab "0.4.2"]
                  [org.babashka/sci "0.3.2"]
-                 [babashka/babashka "0.3.5"]
+                 [babashka/babashka "0.7.7"]
                  [com.taoensso/tower "3.1.0-beta4"]
-                 [com.taoensso/encore "3.19.0"]
+                 [com.taoensso/encore "3.21.0"]
                  [trptcolin/versioneer "0.2.0"]]
 
   :min-lein-version "2.0.0"
@@ -61,8 +61,8 @@
 
    :project/docker  {:source-paths   ["env/docker/clj"]
                      :resource-paths ["env/docker/resources"]}
-   :project/dev     {:dependencies   [[prone "2020-01-17"]
-                                      [org.clojure/test.check "1.1.0"]
+   :project/dev     {:dependencies   [[prone "2021-04-23"]
+                                      [org.clojure/test.check "1.1.1"]
                                       [circleci/circleci.test "0.5.0"]]
                      :source-paths   ["env/dev/clj"]
                      :test-paths     ["test/clj"]
@@ -70,7 +70,7 @@
                      :repl-options   {:init-ns user}}
 
    :project/graal   {:dependencies [[com.github.dblock.waffle/waffle-jna "1.8.1" :exclusions [com.google.guava/guava net.java.dev.jna/jna]]
-                                    [net.java.dev.jna/jna-platform "5.7.0"]
+                                    [net.java.dev.jna/jna-platform "5.10.0"]
                                     [com.ongres.scram/client "2.1"]]}
 
 
